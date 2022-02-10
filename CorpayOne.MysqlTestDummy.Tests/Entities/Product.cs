d950ff58c6245a83d4fa72e31afcd028d638140c
Eliot Jones <eliot@corpayone.com>
2022-02-10 16:24:59 -0400

    add tests with dapper

---
+++ CorpayOne.MysqlTestDummy.Tests/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Dapper.Contrib.Extensions;
+
+namespace CorpayOne.MysqlTestDummy.Tests.Entities
+{
+    [Table("Products")]
+    internal class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        // ReSharper disable once InconsistentNaming
+        public string SKU { get; set; }
+
+        public string Subtitle { get; set; }
+    }
+
+    [Table("Users")]
+    internal class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Country { get; set; }
+    }
+}
--- CorpayOne.MysqlTestDummy.Tests/IntegrationTests.cs
+++ CorpayOne.MysqlTestDummy.Tests/IntegrationTests.cs
@@ -1,3 +1,6 @@
+using System.Threading.Tasks;
+using CorpayOne.MysqlTestDummy.Tests.Entities;
+using Dapper.Contrib.Extensions;
 using Xunit;
 
 namespace CorpayOne.MysqlTestDummy.Tests
@@ -12,11 +15,32 @@ namespace CorpayOne.MysqlTestDummy.Tests
         }
 
         [Fact]
-        public void SimpleIntTableCreate()
+        public async Task SimpleIntId_ProductTable_Creates()
         {
             var conn = _fixture.GetConnection();
 
             var id = Dummy.CreateId<int>(conn, "Products");
+
+            Assert.True(id > 0, $"Expected id to be greater than zero but was {id}.");
+
+            var product = await conn.GetAsync<Product>(id);
+
+            Assert.NotNull(product);
+
+            Assert.Equal(id, product.Id);
+
+            Assert.Null(product.Subtitle);
+            Assert.False(
+                string.IsNullOrWhiteSpace(product.Name),
+                $"Product name should not have been null or whitespace but was: '{product.Name}'");
+        }
+
+        [Fact]
+        public void SimpleIntId_UserTable_Creates()
+        {
+            var conn = _fixture.GetConnection();
+
+            var id = Dummy.CreateId<int>(conn, "Users");
 
             Assert.True(id > 0, $"Expected id to be greater than zero but was {id}.");
         }
